b886dd95f5617e411baf1d6bb75c0d5d17f8ea2e
MyMrshll <ferta966@gmail.com>
2025-03-22 17:48:52 +0700

    response username

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,7 +65,7 @@ const login = async (req, res) => {
     res.json({
       message: "Login successful",
       token,
-      user
+      user : user.username
     });
   } catch (error) {
     console.error(error);
